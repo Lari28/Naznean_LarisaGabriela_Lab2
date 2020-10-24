bcd85a552507f035cf0262b1c1a220d0982804db
Larisa Gabriela Naznean <nazneanlarisa@gmail.com>
2020-10-24 18:49:55 +0300

    Adaugat lab3

--- DoughnutMachine.cs
+++ DoughnutMachine.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.ComponentModel;
+using System.Text;
+using System.Windows.Threading;
 
 namespace Naznean_LarisaGabriela_Lab2
 {
@@ -14,7 +15,7 @@ namespace Naznean_LarisaGabriela_Lab2
             {
                 return mFlavor;
             }
-            set
+               set
             {
                 mFlavor = value;
             }
@@ -25,63 +26,112 @@ namespace Naznean_LarisaGabriela_Lab2
             get
             {
                 return (Doughnut)mDoughnuts[Index];
-
             }
             set
             {
                 mDoughnuts[Index] = value;
             }
         }
+        public delegate void DoughnutCompleteDelegate();
+        public event DoughnutCompleteDelegate DoughnutComplete;
+        DispatcherTimer doughnutTimer;
+        private void InitializeComponent()
+        {
+            this.doughnutTimer = new DispatcherTimer();
+            this.doughnutTimer.Tick += new System.EventHandler(this.doughnutTimer_Tick);
+        }
+
+        public DoughnutMachine()
+        {
+            InitializeComponent();
+        }
+
+        private void doughnutTimer_Tick(object sender, EventArgs e)
+        {
+            Doughnut aDoughnut = new Doughnut(this.Flavor);
+            mDoughnuts.Add(aDoughnut);
+            DoughnutComplete();
+        }
+
+        public bool Enabled
+        {
+            set
+            {
+                doughnutTimer.IsEnabled = value;
+            }
+        }
+        public int Interval
+        {
+            set
+            {
+                doughnutTimer.Interval = new TimeSpan(0, 0, value);
+            }
+        }
+
+        public void MakeDoughnuts(DoughnutType dFlavor)
+        {
+
+            Flavor = dFlavor;
+            switch (Flavor)
+            {
+                case DoughnutType.Glazed: Interval = 3; break;
+                case DoughnutType.Sugar: Interval = 2; break;
+                case DoughnutType.Lemon: Interval = 5; break;
+                case DoughnutType.Chocolate: Interval = 7; break;
+                case DoughnutType.Vanilla: Interval = 4; break;
+            }
+            doughnutTimer.Start();
+        }
+    }
+    public enum DoughnutType
+    {
+        Glazed,
+        Sugar,
+        Lemon,
+        Chocolate,
+        Vanilla
+    }
+
+    class Doughnut
+    {
+        private DoughnutType mFlavor;
+
+        public DoughnutType Flavor
+        {
+            get
+            {
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
+        }
+        private float mPrice = .50F;
+        public float Price
+        {
+            get
+            {
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
+        private readonly DateTime mTimeOfCreation;
+        public DateTime TimeOfCreation
+        {
+            get
+            {
+                return mTimeOfCreation;
+            }
+
+        }
+        public Doughnut(DoughnutType aFlavor) // constructor
+        {
+            mTimeOfCreation = DateTime.Now;
+            mFlavor = aFlavor;
+        }
     }
 }
-public enum DoughnutType
-{
-    Glazed,
-    Sugar,
-    Lemon,
-    Chocolate,
-    Vanilla
-}
-
-class Doughnut
-{
-    private DoughnutType mFlavor;
-    public DoughnutType Flavor
-    {
-        get
-        {
-            return mFlavor;
-        }
-        set
-        {
-            mFlavor = value;
-        }
-    }
-    private float mPrice = .50F;
-    public float Price
-    {
-        get
-        {
-            return mPrice;
-        }
-        set
-        {
-            mPrice = value;
-        }
-    }
-
-    private readonly DateTime mTimeOfCreation;
-    public DateTime TimeOfCreation
-    {
-        get
-        {
-            return mTimeOfCreation;
-        }
-    }
-    public Doughnut(DoughnutType aFlavor)
-    {
-        mTimeOfCreation = DateTime.Now;
-        mFlavor = aFlavor;
-    }
-}
-
